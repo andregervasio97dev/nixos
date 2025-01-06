c59f3bebdf0176e017f6d3adea5caa929de49f21
Illyanda <andregervasio97dev@gmail.com>
2025-01-06 16:53:38 -0300

    setup wireplumber

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -55,7 +55,7 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver.enable = false;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager = {
@@ -81,13 +81,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+	wireplumber.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
