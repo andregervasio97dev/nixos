2f2c06ad562a2d0347654b3e30c6c1a988525524
Illyanda <andregervasio97dev@gmail.com>
2025-01-06 17:01:33 -0300

    wayland fun times

--- modules/home-manager/compositors/hyprland/hyprland.nix
+++ modules/home-manager/compositors/hyprland/hyprland.nix
@@ -196,13 +196,7 @@
 		"noinitialfocus, class:^(xwaylandvideobridge)$"
 		"maxsize 1 1, class:^(xwaylandvideobridge)$"
 		"noblur, class:^(xwaylandvideobridge)$"
-		"nofocus, class:^(xwaylandvideobridge)$
-
-
-
-
-
-
+		"nofocus, class:^(xwaylandvideobridge)$"
 	  ];
 	};
   };
