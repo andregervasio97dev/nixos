36ecc9d02740c38c55a62ab8b4d71e591f0b8d13
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 09:24:47 -0300

    Waybar testing

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -6,12 +6,6 @@
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
   environment.systemPackages = with pkgs; [
-    # Bar for wayland
-    waybar
-    (waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-      })
-    )
     # app launcher 
     wofi
     # Notifier daemon
