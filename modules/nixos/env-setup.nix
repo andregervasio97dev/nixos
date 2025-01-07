bbe3db28c043764a5b629788d925992af5f2203f
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 17:45:42 -0300

    Random wallpaper

--- modules/nixos/env-setup.nix
+++ modules/nixos/env-setup.nix
@@ -60,4 +60,8 @@
     powerline-fonts
   ];
   security.sudo.configFile = "Defaults env_keep+=SSH_AUTH_SOCK";
+  services.cron = {
+	enable = true;
+	systemCronJobs = [ "*/1* * * * *	zsh $HOME/Scripts/set_random_wallpaper.sh >> /dev/null" ];
+  };
 }
