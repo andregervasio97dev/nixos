ef8cb4e3e695c60f8c346c7a342332862435d9ee
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 20:21:57 -0300

    gave up

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,6 +1,10 @@
 { pkgs, inputs, ... }:
-
-{
+let
+	wallpaper-random = pkgs.writeShellScriptBin "wallpaper-random"
+	''
+		bash /home/illyanda/Scripts/set_random_wallpaper.sh >> /dev/null
+	'';
+in {
 	programs.hyprland = {
 		enable = true;
 		package = inputs.hyprland.packages."${pkgs.system}".hyprland;
@@ -26,5 +30,24 @@
 		extraPortals = [ 
 			pkgs.xdg-desktop-portal-gtk 
 		];
+	};
+	systemd.timers."wallpaper-random" = {
+		wantedBy = [ "timers.target" ];
+		timerConfig = {
+			OnBootSec = "10s";
+			OnUnitActiveSec = "10s";
+			Unit = "wallpaper-random.service";
+		};
+	};
+
+	systemd.services."wallpaper-random" = {
+		script = ''
+			set -eu
+			${wallpaper-random}/bin/wallpaper-random
+			'';
+		serviceConfig = {
+			Type = "oneshot";
+			User = "root";
+		};
 	};
 }
