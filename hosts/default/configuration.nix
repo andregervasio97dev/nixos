357f04ee2492a7067192ad6263859bdddbf33d88
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 07:15:33 -0300

    changed numlock config

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -62,81 +62,81 @@
     sddm = {
 		enable = true;
 		autoNumlock = true;
-	}
+	};
 	defaultSession = "hyprland";
   };
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
+# Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+	  layout = "us";
+	  variant = "";
   };
 
-  # Enable CUPS to print documents.
+# Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+# Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-	wireplumber.enable = true;
+	  enable = true;
+	  alsa.enable = true;
+	  alsa.support32Bit = true;
+	  pulse.enable = true;
+	  jack.enable = true;
+	  wireplumber.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
- 
-  # Install firefox.
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
 
-  # Allow unfree packages
+# Install firefox.
+
+# Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   home-manager = {
-    backupFileExtension = "backup";
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "illyanda" = import ./home.nix;
-    };
+	  backupFileExtension = "backup";
+	  extraSpecialArgs = { inherit inputs; };
+	  users = {
+		  "illyanda" = import ./home.nix;
+	  };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# List packages installed in system profile. To search, run:
+# $ nix search wget
 
-  # List services that you want to enable:
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
   hardware.bluetooth.enable = true;
 
-  # Enable the OpenSSH daemon.
+# Enable the OpenSSH daemon.
   services.openssh.enable = true;
   security.pam = {
-    rssh.enable = true;
-    sshAgentAuth.enable = true;
+	  rssh.enable = true;
+	  sshAgentAuth.enable = true;
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
 }
