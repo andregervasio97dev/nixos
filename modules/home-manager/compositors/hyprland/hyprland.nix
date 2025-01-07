96e089724e368af9e10146ca4cb28d38edcabba3
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 13:22:28 -0300

    waybar

--- modules/home-manager/compositors/hyprland/hyprland.nix
+++ modules/home-manager/compositors/hyprland/hyprland.nix
@@ -21,7 +21,7 @@
 					"tray"
 				];
 				"clock" = {
-					format = "{:%H:%M - %d/%m/%Y}";
+					format = "{  :%H:%M - %d/%m/%Y}";
 					tooltip-format = "<tt><small>{calendar}</small></tt>";
 					calendar = {
 						mode = "year";
