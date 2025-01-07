65792e23a72bbad69bf0e17cd1784bb3e4ffea60
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 16:50:46 -0300

    Added random wallpaper on boot

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -1,30 +1,50 @@
-{ config, pkgs, inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
-  programs.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-  };
-  environment.systemPackages = with pkgs; [
-    # app launcher 
-    wofi
-    # Notifier daemon
-    dunst
-    # dependancy for dunst
-    libnotify
-    # wallpaper daemon
-    hyprpaper
-    # GUI for network
-    networkmanagerapplet
+	programs.hyprland = {
+		enable = true;
+		package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+	};
 
-	qt5.qtwayland
-	qt6.qtwayland
+	environment.systemPackages = with pkgs; [
+		# app launcher 
+		wofi
+		# Notifier daemon
+		dunst
+		# dependancy for dunst
+		libnotify
+		# wallpaper daemon
+		hyprpaper
+		# GUI for network
+		networkmanagerapplet
+		qt5.qtwayland
+		qt6.qtwayland
+	];
 
-  ];
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ 
-      pkgs.xdg-desktop-portal-gtk 
-    ];
-  };
+	xdg.portal = {
+		enable = true;
+		extraPortals = [ 
+			pkgs.xdg-desktop-portal-gtk 
+		];
+	};
+
+	systemd.services."set-random-wallpaper" = {
+		script = ''
+			set -eu
+			zsh $HOME/Scripts/set_random_wallpaper.sh
+		'';
+		serviceConfig = {
+			Type = "oneshot";
+			User = "illyanda";
+		};
+	};
+
+	systemd.timers."set_random_wallpaper" = {
+		wantedBy = [ "timers.target" ];
+		timerConfig = {
+			OnBootSec = "30s";
+			OnUnitActiveSec = "30m";
+			Unit = "set_random_wallpaper.service";
+		};
+	};
 }
