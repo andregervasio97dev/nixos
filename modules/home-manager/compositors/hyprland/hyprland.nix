8c679cf5a92ff3da6d73fa4d7196e34782fb418f
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 14:59:51 -0300

    waybar

--- modules/home-manager/compositors/hyprland/hyprland.nix
+++ modules/home-manager/compositors/hyprland/hyprland.nix
@@ -1,6 +1,10 @@
 { ... }:
 let
-	textColor = "rgb(255, 255, 255)";
+	mainColors = {
+		textColor = "rgb(255, 255, 255)";
+		background-color = "rgba(140, 140, 140, 0.6)";
+	};
+
 in {
 	programs.waybar = {
 		enable = true;
@@ -99,18 +103,17 @@ in {
 		style = ''
 			window#waybar {
 				background-color: rgba(255, 255, 255, 0);
-				color: rgb(255, 255, 255);
 			}
 			#workspaces button {
 				margin: 0 2px;
 				padding: 0 5px;
 				border: 1px solid rgba(110, 110, 110, 0.8);
 				border-radius: 5px;
-				background-color: rgba(80, 80, 80, 0.8);
-				color: rgb(30, 30, 30);
+				background-color: ${mainColors.background-color};
+				color: ${mainColors.textColor};
 			}
 			#workspaces button.active {
-				color: ${textColor};
+				color: ${mainColors.textColor};
 			}
 
 			#clock,
@@ -123,17 +126,17 @@ in {
 			#bluetooth {
 				margin: 0 2px;
 				padding: 0 5px;
-				border: 1px solid rgba(110, 110, 110, 0.8);
+				border: 1px solid rgba(200, 200, 200, 0.8);
 				border-radius: 5px;
-				background-color: rgba(40, 40, 40, 0.6);
-				color: rgb(255, 255, 255);
+				background-color: ${mainColors.background-color};
+				color: ${mainColors.textColor};
 			}
 			#tray {
 				margin: 0 2px;
 				padding: 0 5px;
 				border: 1px solid rgba(40, 40, 40, 0.8);
 				border-radius: 5px;
-				background-color: rgba(110, 110, 110, 0.6);
+				background-color: ${mainColors.background-color};
 				color: rgb(255, 255, 255);
 			}
 		'';
