020db6b9026d37f348922f40a3045188436f6070
Illyanda <andregervasio97dev@gmail.com>
2025-01-07 23:02:13 -0300

    reorganized

--- modules/home-manager/shells/zsh/zsh.nix
+++ modules/home-manager/shells/zsh/zsh.nix
@@ -21,5 +21,4 @@
 			];
 		};
 	};
-	environment.pathsToLink = [ "/share/zsh" ];
 }
--- modules/nixos/env-setup.nix
+++ modules/nixos/env-setup.nix
@@ -32,6 +32,7 @@
 			TERM = "kitty";
 			NIXOS_OZONE_WL = "1";
 		};
+		pathsToLink = [ "/share/zsh" ];
 	};
 	programs = {
 		zsh = {
