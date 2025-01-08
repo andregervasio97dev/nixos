98dfe7b7d47f4dfdeca4cc08f55fa2b65af72e69
Illyanda <andregervasio97dev@gmail.com>
2025-01-08 09:13:05 -0300

    waybar workspaces space fixing

--- modules/home-manager/gui/compositors/hyprland.nix
+++ modules/home-manager/gui/compositors/hyprland.nix
@@ -13,8 +13,9 @@ let
 		textColor = "rgb(66, 245, 108)";
 	};
 	fontSizes = {
-		mainSize = "16px";
-		midSize = "12px";
+		mainSize = "12px";
+		midSize = "10px";
+		smallSize = "8px";
 	};
 in {
 	programs.waybar = {
