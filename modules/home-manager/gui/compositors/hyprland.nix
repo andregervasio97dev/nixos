0e59a4755f7c33f4ee80ff89d5a28c47b6ef9436
Illyanda <andregervasio97dev@gmail.com>
2025-01-08 07:40:39 -0300

    waybar workspaces space fixing

--- modules/home-manager/gui/compositors/hyprland.nix
+++ modules/home-manager/gui/compositors/hyprland.nix
@@ -119,7 +119,7 @@ in {
 			}
 			#workspaces button {
 				margin: 0 2px;
-				padding: 5 10px;
+				padding: 3px 10px;
 				border: ${mainColors.border};
 				border-radius: 5px;
 				background-color: ${mainColors.background-color};
