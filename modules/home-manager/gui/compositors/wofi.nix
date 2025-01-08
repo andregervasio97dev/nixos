38998debef4b4e3716fbed3c30d6766372a78c70
Illyanda <andregervasio97dev@gmail.com>
2025-01-08 14:54:27 -0300

    wofi

--- modules/home-manager/gui/compositors/wofi.nix
+++ modules/home-manager/gui/compositors/wofi.nix
@@ -1,17 +1,62 @@
 { ... }:
+let
+	gradientColors = {
+		first = "rgb(54, 1, 103)";
+		second = "rgb(107, 7, 114)";
+		third = "rgb(175, 7, 114)";
+		fourth = "rgb(207, 38, 138)";
+		fifth = "rgb(230, 38, 156)";
+		sixth = "rgb(251, 140, 171)";
+		fullGradient = "linear-gradient(
+			to right bottom,
+			${gradientColors.first},
+			${gradientColors.second},
+			${gradientColors.third},
+			${gradientColors.fourth},
+			${gradientColors.fifth},
+			${gradientColors.sixth}
+		)";
+		firstHex = "360167";
+		sixthHex = "FB8CAB";
+	};
 
-{
+	contrastColors = {
+		main = "rgb(252, 199, 55)";
+		accent = "rgb(242, 107, 15)";
+	};
+
+	textColors = {
+		active = "rgb(255, 255, 255)";
+		focused = "${contrastColors.main}"; 
+		unfocused = "rgb(150, 150, 150)";
+	};
+in {
 	programs.wofi = {
 		enable = true;
 		settings = {
 			prompt = "";
 			sort_order = "alphabetical";
-			key_left = "h";
-			key_down = "j";
-			key_up = "k";
-			key_right = "l";
 		};
 		style = ''
+			window {
+				font-size: 14px;
+				color: ${textColors.focused}
+				background: linear-gradient(
+					rgba(150, 150, 150, 0.5),
+					rgba(150, 150, 150, 0.5)
+					) padding-box,
+					${gradientColors.fullGradient} border-box;
+				border: 2px solid transparent;
+			}
+			#input {
+				color: ${contrastColors.main};
+			}
+			#entry:selected {
+				background-color: ${gradientColors.third};
+			}
+			#text:selected {
+				color: ${textColors.active};
+			}
 		'';
 	};
 }
