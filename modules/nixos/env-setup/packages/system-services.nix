cdc0343a1471004f07a493762b4f22c6e7db41bc
Illyanda <andregervasio97dev@gmail.com>
2025-01-11 14:34:40 -0300

    keyboards tests

--- modules/nixos/env-setup/packages/system-services.nix
+++ modules/nixos/env-setup/packages/system-services.nix
@@ -18,16 +18,34 @@
 				kanata.config = ''
 					(defsrc
 						caps
+						a s d f
+						h j k l
 					)
 					(defvar
 						tap-time 200
 						hold-time 200
 					)
 					(defalias
-						caps (tap-hold $tap-time $hold-time esc caps)
+						caps-mod (tap-hold $tap-time $hold-time esc caps)
+						a-mod (tap-hold $tap-time $hold-time a lsft)
+						s-mod (tap-hold $tap-time $hold-time s ctl)
+						d-mod (tap-hold $tap-time $hold-time d lmet)
+						f-mod (tap-hold $tap-time $hold-time f lalt)
+						h-mod (tap-hold $tap-time $hold-time h lalt)
+						j-mod (tap-hold $tap-time $hold-time j rmet)
+						k-mod (tap-hold $tap-time $hold-time k rctl)
+						l-mod (tap-hold $tap-time $hold-time l rsft)
 					)
 					(deflayer base
-						@caps
+						@caps-mod
+						@a-mod
+                        @s-mod
+                        @d-mod
+                        @f-mod
+                        @h-mod
+                        @j-mod
+                        @k-mod
+                        @l-mod
 					)
 				'';
 			};
