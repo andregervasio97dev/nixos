6adf58adb8221f0cf9aaeb9beecd75f0a775ebb8
Illyanda <andregervasio97dev@gmail.com>
2025-01-28 13:48:44 -0300

    Bugtesting Hyprland

--- modules/home-manager/gui/compositors/hyprland.nix
+++ modules/home-manager/gui/compositors/hyprland.nix
@@ -442,7 +442,7 @@ wayland.windowManager.hyprland = {
 			"suppressevent maximize, class:.*"
 			"opacity 0.0 override, class:^(xwaylandvideobridge)$"
 			"noanim, class:^(xwaylandvideobridge)$"
-			"noinitialfocus, class:^(xwaylandvideobridge)$"
+			#"noinitialfocus, class:^(xwaylandvideobridge)$"
 			"maxsize 1 1, class:^(xwaylandvideobridge)$"
 			"noblur, class:^(xwaylandvideobridge)$"
 			# Defining workspaces for certain windows
