d610e0b7cee0a86235091253963c5b3ec30f6305
Illyanda <andregervasio97dev@gmail.com>
2025-01-29 14:05:22 -0300

    Added extra layers to kanata

--- modules/nixos/env-setup/packages/system-services.nix
+++ modules/nixos/env-setup/packages/system-services.nix
@@ -17,15 +17,18 @@
 				kanata.extraDefCfg = "process-unmapped-keys yes";
 				kanata.config = ''
 					(defsrc
-						caps
-						a s d f
-						j k l ;
+  						grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  						tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+  						caps a    s    d    f    g    h    j    k    l    ;    '    ret
+  						lsft z    x    c    v    b    n    m    ,    .    /    rsft
+  						lctl lmet lalt           spc            ralt rmet rctl
 					)
 					(defvar
 						tap-time 200
 						hold-time 200
 					)
 					(defalias
+						tab-mod (tap-hold $tap-time $hold-time tab layer-toggle movement)
 						caps-mod (tap-hold $tap-time $hold-time esc caps)
 						a-mod (tap-hold $tap-time $hold-time a lsft)
 						s-mod (tap-hold $tap-time $hold-time s ctl)
@@ -36,16 +39,19 @@
 						l-mod (tap-hold $tap-time $hold-time l rctl)
 						;-mod (tap-hold $tap-time $hold-time ; rsft)
 					)
-					(deflayer base
-						@caps-mod
-						@a-mod
-                        @s-mod
-                        @d-mod
-                        @f-mod
-                        @j-mod
-                        @k-mod
-                        @l-mod
-                        @;-mod
+					(deflayer qwerty
+						grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+						@tab-mod  q    w    e    r    t    y    u    i    o    p    [    ]    \
+						@caps-mod @a-mod    @s-mod    @d-mod    @f-mod    h    @j-mod    @k-mod    @l-mod    @;-mod    '    ret
+						lsft z    x    c    v    b    n    m    ,    .    /    rsft
+						lctl lmet lalt           spc            ralt rmet rctl
+					)
+					(deflayer movement
+						_    _    _    _    _    _    _    _    _    _    _    _    _    _
+						_    _    _    _    _    _    _    _    _    _    _    _    _    _
+						_    _    _    _    _    lft  down up   rght _    _    _    _
+						_    _    _    _    _    _    _    _    _    _    _    _
+						_    _    _              _              _    _    _
 					)
 				'';
 			};
